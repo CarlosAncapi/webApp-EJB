a03fb13d9130e9556871d721eb9697cc3dddeda3
Carlos Ancapi <carlos.ancapi@gmail.com>
2023-04-18 17:11:46 -0400

    3
    obteniendo EJB programaticamente usando InitialContext lookup con jndi

--- src/main/java/org/cancapi/webapp/ejb/controllers/EjemploServlet.java
+++ src/main/java/org/cancapi/webapp/ejb/controllers/EjemploServlet.java
@@ -11,19 +11,32 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.cancapi.webapp.ejb.service.ServiceEJB;
 
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.io.IOException;
 
 @WebServlet("/index")
 public class EjemploServlet extends HttpServlet {
 
-    @Inject
-    private ServiceEJB service;
+    //@Inject
+    //private ServiceEJB service;
 
-    @Inject
-    private ServiceEJB service2;
+    //@Inject
+    //private ServiceEJB service2;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ServiceEJB service = null;
+        ServiceEJB service2 = null;
+
+        try {
+            InitialContext ctx = new InitialContext();
+            service = (ServiceEJB) ctx.lookup("java:global/webapp-ejb/ServiceEJB!org.cancapi.webapp.ejb.service.ServiceEJB");
+            service2 = (ServiceEJB) ctx.lookup("java:global/webapp-ejb/ServiceEJB!org.cancapi.webapp.ejb.service.ServiceEJB");
+        } catch (NamingException e){
+            e.printStackTrace();
+        }
 
         System.out.println("service si es igual a service2 = " + service.equals(service2) );
         req.setAttribute("saludo", service.saludar("Andres"));
